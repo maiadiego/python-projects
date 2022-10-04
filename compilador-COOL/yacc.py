e09dfefde73f3ec16bb6f6fe939cf605c11c8de7
Guilherme <guilhermecsilva8@gmail.com>
2022-10-04 11:39:26 -0300

    reorganizando as regras

--- compilador-COOL/yacc.py
+++ compilador-COOL/yacc.py
@@ -30,32 +30,24 @@ def p_classes(p):
 
 
 def p_class(p):
-    """class : CLASS TYPE inherits LBRACE features_opt RBRACE DOTCOMMA"""
+    """class : CLASS TYPE inherits LBRACE features RBRACE DOTCOMMA"""
      
-
-
-def p_features_opt(p):
-    """features_opt : features
-                    | empty"""
 
 def p_features(p):
     """features : feature
-                | feature features"""
+                | feature features
+                | empty"""
    
 
 def p_feature(p):
-    """feature : ID LPAREN formals_opt RPAREN DOUBLEDOT TYPE LBRACE expr RBRACE DOTCOMMA
+    """feature : ID LPAREN formals RPAREN DOUBLEDOT TYPE LBRACE expr RBRACE DOTCOMMA
                | attr_def DOTCOMMA"""
-
-
-def p_formals_opt(p):
-    """formals_opt : formals
-                   | empty"""
 
 
 def p_formals(p):
     """formals : formal
-               | formal COMMA formals"""
+               | formal COMMA formals
+               | empty"""
 
 
 def p_formal(p):
@@ -63,11 +55,12 @@ def p_formal(p):
     
 
 def p_expr(p):
-    """expr : ID assign                                      
-            | expr targettype_opt DOT function_call          
+    """expr : ID ARROW expr                                      
+            | expr targettype DOT function_call          
             | function_call                                  
             | IF expr THEN expr ELSE expr FI                
-            | WHILE expr LOOP expr POOL                                      
+            | WHILE expr LOOP expr POOL      
+            | LBRACE blockelements RBRACE                                
             | LET attr_defs IN expr                              
             | CASE expr OF typeactions ESAC                  
             | NEW TYPE
@@ -80,7 +73,6 @@ def p_expr(p):
             | expr LT expr
             | expr LE expr
             | expr EQ expr
-            | LBRACE block RBRACE
             | NOT expr                     
             | LPAREN expr RPAREN
             | ID
@@ -91,17 +83,13 @@ def p_expr(p):
 
 # regras auxiliares  
 
-def p_targettype_opt(p):
-    """targettype_opt : targettype
-                      | empty"""
-
-
 def p_targettype(p):
-    """targettype : AT TYPE"""
+    """targettype : AT TYPE
+                  | empty"""
     
 
 def p_function_call(p):
-    """function_call : ID LPAREN params_opt RPAREN"""
+    """function_call : ID LPAREN params RPAREN"""
 
 
 def p_attr_defs(p):
@@ -110,7 +98,7 @@ def p_attr_defs(p):
 
 
 def p_attr_def(p):
-    """attr_def : ID DOUBLEDOT TYPE assign_opt"""
+    """attr_def : ID DOUBLEDOT TYPE assign"""
 
 
 def p_typeactions(p):
@@ -125,29 +113,17 @@ def p_typeaction(p):
 def p_inherits(p):
     """inherits : INHERITS TYPE
                 | empty"""
-   
-
-def p_assign_opt(p):    
-    """assign_opt : assign
-                  | empty"""
 
 
 def p_assign(p):
-    """assign : ARROW expr"""
-   
-
-def p_params_opt(p):
-    """params_opt : params
-                  | empty"""
+    """assign : ARROW expr
+              | empty"""
 
 
 def p_params(p):
     """params : expr
-              | expr COMMA params"""
-
-
-def p_block(p):
-    """block : blockelements"""
+              | expr COMMA params
+              | empty"""
 
 
 def p_blockelements(p):
