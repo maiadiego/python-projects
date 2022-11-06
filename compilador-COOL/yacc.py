0911589791633d59d13669a1eeaa8df0d935bc50
maiadiego <diegomaiaqq@gmail.com>
2022-11-06 14:28:24 -0300

    criando a árvore sintática abstrata

--- compilador-COOL/yacc.py
+++ compilador-COOL/yacc.py
@@ -1,6 +1,48 @@
 import ply.yacc as yacc
 from calclex import tokens  
+from collections import namedtuple
+from types import new_class
 
+
+class Returnable:
+    return_type = None
+
+def returnable_namedtuple(type_name, fields):
+    return new_class(type_name, (Returnable, namedtuple(type_name, fields)))
+
+
+# namedtuple é uma forma concisa para criar classes que representam os nós 
+# da árvore sintática abstrata, de acordo com cada regra
+# new_class cria um objeto de classe dinamicamente passando o nome e as definições do cabeçalho da classe
+
+Class = namedtuple("Class", "name, parent, feature_list")
+Method = returnable_namedtuple("Method", "name, formal_list, return_type, body") 
+Id = returnable_namedtuple('Id', 'name')
+Assign = returnable_namedtuple("Assign", "name, body")
+MethodCall = returnable_namedtuple('MethodCall', 'object targettype method')
+If = returnable_namedtuple("If", "predicate, then_body, else_body")
+While = returnable_namedtuple("While", "predicate, body")
+Block = returnable_namedtuple("Block", "body")
+Let = returnable_namedtuple('Let', 'assignments expr')
+Case = returnable_namedtuple('Case', "expr, typeactions")
+New = returnable_namedtuple("New", "type")
+Isvoid = returnable_namedtuple("Isvoid", "body")
+Not = returnable_namedtuple("Not", "body")
+Complement = returnable_namedtuple("Complement", "body") 
+Int = returnable_namedtuple("Int", "content")
+Bool = returnable_namedtuple("Bool", "content")
+Str = returnable_namedtuple("Str", "content")
+Plus = returnable_namedtuple("Plus", "first, second")
+Sub = returnable_namedtuple("Sub", "first, second")
+Mult = returnable_namedtuple("Mult", "first, second")
+Div = returnable_namedtuple("Div", "first, second")
+Lt = returnable_namedtuple("Lt", "first, second")
+Le = returnable_namedtuple("Le", "first, second")
+Eq = returnable_namedtuple("Eq", "first, second")
+FunctionCall = returnable_namedtuple('FunctionCall', 'id params')
+Attr = namedtuple("Attr", "name, type, body")
+TypeAction = returnable_namedtuple('TypeAction', 'id type expr')
+Self = returnable_namedtuple('Self','id')
 
 precedence = (
     ('right', 'ARROW'),
@@ -21,25 +63,21 @@ precedence = (
 # regras de acordo com a ordem do manual de cool
 # Ctrl K + Ctrl C pra comentar em bloco e Ctrl K + Ctrl U para descomentar
 
-def p_program(p):
-    """program : classes"""
-    p[0] = p[1] 
+def p_class_list_many(p):
+    """class_list : class_list class DOTCOMMA"""
+    p[0] = p[1] + [p[2]]
 
-
-def p_classes(p):
-    """classes : class
-               | class classes"""
-    if len(p) == 2:
-        p[0] = [p[1]]
-    elif len(p) == 3:
-        p[0] = p[1] + p[[2]]
-    else:
-        raise SyntaxError('Número de símbolos inválido')
-
+def p_class_list_single(p):
+    """class_list : class DOTCOMMA"""
+    p[0] = [p[1]]
 
 def p_class(p):
-    """class : CLASS TYPE inherits LBRACE features RBRACE DOTCOMMA"""
-    p[0] = Class(p[2], p[3], p[5])
+    """class : CLASS TYPE LBRACE features RBRACE"""
+    p[0] = Class(p[2], "Id", p[4])
+
+def p_class_inherits(p):
+    """class : CLASS TYPE INHERITS TYPE LBRACE features RBRACE"""
+    p[0] = Class(p[2], p[4], p[6])
      
 
 def p_features(p):
@@ -60,6 +98,13 @@ def p_features(p):
 def p_feature(p):
     """feature : ID LPAREN formals RPAREN DOUBLEDOT TYPE LBRACE expr RBRACE DOTCOMMA
                | attr_def DOTCOMMA"""
+    
+    if len(p) == 11:
+        p[0] = Method(p[1], p[3], p[6], p[8])
+    elif len(p) == 3:
+        p[0] = p[1]
+    else:
+        raise SyntaxError('Número de símbolos inválido')
 
 
 def p_formals(p):
@@ -116,10 +161,12 @@ def p_expr(p):
     if first_token == 'ID':
         if second_token is None:
             p[0] = Id(p[1])
-        elif second_token == 'assign':
-            p[0] = idAssign(p[1], p[3])  # não tenho certeza
+        elif second_token == 'ARROW':
+            p[0] = Assign(p[1], p[3])  
     
-    # falta a segunda regra
+    elif first_token == 'expr':
+        if third_token == 'DOT':
+            p[0] = MethodCall(p[1], p[2], p[4])
 
     elif first_token == 'function_call':
         p[0] = p[1]
@@ -167,7 +214,8 @@ def p_expr(p):
         p[0] = Eq(p[1], p[3])
     
 
-# regras auxiliares  
+# regras auxiliares 
+
 def p_targettype(p):
     """targettype : AT TYPE
                   | empty"""
@@ -181,6 +229,7 @@ def p_targettype(p):
 
 def p_function_call(p):
     """function_call : ID LPAREN params RPAREN"""
+    p[0] = FunctionCall(p[1], p[3])
 
 
 def p_attr_defs(p):
@@ -196,6 +245,7 @@ def p_attr_defs(p):
 
 def p_attr_def(p):
     """attr_def : ID DOUBLEDOT TYPE assign"""
+    p[0] = Attr(p[1], p[3], p[4])
 
 
 def p_typeactions(p):
@@ -211,17 +261,18 @@ def p_typeactions(p):
 
 def p_typeaction(p):
     """typeaction : ID DOUBLEDOT TYPE EL expr DOTCOMMA"""
+    p[0] = TypeAction(p[1], p[3], p[5])
 
 
-def p_inherits(p):
-    """inherits : INHERITS TYPE
-                | empty"""
-    if len(p) == 2:
-        p[0] = p[1]
-    elif len(p) == 3:
-        p[0] = p[2]
-    else:
-        raise SyntaxError('Número de símbolos inválido')
+# def p_inherits(p):
+#     """inherits : INHERITS TYPE
+#                 | empty"""
+#     if len(p) == 2:
+#         p[0] = p[1]
+#     elif len(p) == 3:
+#         p[0] = p[2]
+#     else:
+#         raise SyntaxError('Número de símbolos inválido')
 
 
 def p_assign(p):
@@ -262,6 +313,7 @@ def p_blockelements(p):
 
 def p_expr_self(p):
     """expr  : SELF"""
+    p[0] = Self(p[1])
 
     
 def p_empty(p):
